b3f18d6161a3459836a42f64fc4e217c6f766414
Adam Ghanem <adam.ghanem.it@gmail.com>
2026-08-21 03:05:34 +0100

    feat(swarm): expose additive M4 swarm API

---
+++ packages/swarm/src/m4-index.ts
@@ -0,0 +1,3 @@
+export { M4Swarm, createM4Swarm } from './m4-swarm.js';
+export { chooseAdaptiveTopology, planHierarchical, planMesh, eligibleAgents } from './m4-topology.js';
+export type { M4SwarmAssignment, M4SwarmConfig, M4SwarmEvent, M4SwarmMetrics, M4SwarmStatus, M4SwarmTask, M4SwarmTopology } from './m4-types.js';
